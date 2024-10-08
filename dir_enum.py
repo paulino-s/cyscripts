39c760824d49c8015c6ca703f174e6f050401bd5
PaulinoVasquez <39577327+paulino-s@users.noreply.github.com>
2024-10-08 08:47:28 -0600

    Create dir_enum.py
    add script for directory enumeration

---
+++ dir_enum.py
@@ -0,0 +1,29 @@
+import os
+import requests
+import sys
+
+#Verificaci[n de argumentos
+if len(sys.argv) != 3:
+    sys.exit(1)
+
+file = sys.argv[1]
+path = os.path.join(os.getcwd(), file)
+
+try:
+    with open(path, 'r') as f:
+        sub_list = f.read()
+except FileNotFoundError:
+    print(f"El archivo {file} no ha sido encontrado en la ruta.")
+    sys.exit(1)
+
+directories = sub_list.splitlines()
+
+#Compruebaci[on de directorios
+for dir in directories:
+    dir_enum = f"http://{sys.argv[2]}/{dir}.html"
+    try:
+        r = requests.get(dir_enum)
+        if r.status_code != 404:
+            print("Directorio válido:", dir_enum)
+    except requests.ConnectionError:
+        print(f"No se pudo conectar a {dir_enum}.")
